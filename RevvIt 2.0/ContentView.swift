5996597ebd207d1e86219f568b815dd83384e614
Serenitybleps <fawkesphoenix21@gmail.com>
2023-07-18 12:02:36 -0700

    random button and back color done 1.3

--- RevvIt 2.0/ContentView.swift
+++ RevvIt 2.0/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     
     @State private var promptRandom = ""
     
-        var prompts = ["prompt 1", "prompt 2", "prompt 3", "prompt 4", "prompt 5", "prompt 6"
+        let prompts = ["Talk About the World", "Spend 15 minutes talking about turtles", "talk about your childhood home", "why did you open the app", "are you going to see the new barbie movie", "chicken"
                     
     ]
 
@@ -20,18 +20,33 @@ struct ContentView: View {
     var body: some View {
        
         ZStack {
+          
+            Color(hex: "#F6EFE8")
+                .ignoresSafeArea()
+         
+            
+         
+            
             
             VStack {
                 
                 Text("Journal")
                     .multilineTextAlignment(.leading)
                 
-                
+
                 
                 Button("New Prompt") {
-//                    var randomPrompt = Int.random (
                     
-                }
+                    
+                    let random = Int.random (in:1..<6)
+                    let prompt = prompts[random]
+                    promptRandom = prompt
+            
+
+                    
+                } //button
+                Text (promptRandom)
+                
                 
                 HStack {
                     
@@ -59,8 +74,41 @@ struct ContentView: View {
     }
 }
 
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+    
+    
 }
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (1, 1, 1, 0)
+        }
+
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue:  Double(b) / 255,
+            opacity: Double(a) / 255
+        )
+    }
+}
+
+

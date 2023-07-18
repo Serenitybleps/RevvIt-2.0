ddcfc0fd4a3d3e94f6d41f44558fa741ad895ef1
Serenitybleps <fawkesphoenix21@gmail.com>
2023-07-18 10:15:22 -0700

    Initial Commit

---
+++ RevvIt 2.0/RevvIt_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  RevvIt_2_0App.swift
+//  RevvIt 2.0
+//
+//  Created by scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct RevvIt_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
